d7abba01feece941ff4c19ac1fd67907cd48b22e
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-17 08:52:15 -0200

    Uses Instance interface to Inject the EmailSender bean dynamically based on User OptIn

--- cdi-lazy-injection/src/main/java/com/craftcoder/cdi/produces/Checkout.java
+++ cdi-lazy-injection/src/main/java/com/craftcoder/cdi/produces/Checkout.java
@@ -1,11 +1,12 @@
 package com.craftcoder.cdi.produces;
 
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 public class Checkout {
 
 	@Inject
-	private EmailSender emailSender;
+	private Instance<EmailSender> emailSender;
 
 	@Inject
 	private MetricCreator metricCreator;
@@ -14,7 +15,7 @@ public class Checkout {
 		System.out.println("Finishing Checkout");
 
 		if (user.isOptIn()) { //yes, just Optin users!
-			emailSender.sendEmailFor(user);
+			emailSender.get().sendEmailFor(user);
 		}
 
 		metricCreator.createMetric();
