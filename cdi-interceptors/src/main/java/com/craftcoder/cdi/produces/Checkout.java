4fe500fe98072f992650a6e1159f60858130a50f
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-17 09:45:33 -0200

    Uses @Auditable annotation on Checkout

--- cdi-interceptors/src/main/java/com/craftcoder/cdi/produces/Checkout.java
+++ cdi-interceptors/src/main/java/com/craftcoder/cdi/produces/Checkout.java
@@ -5,15 +5,11 @@ import javax.inject.Inject;
 public class Checkout {
 
 	@Inject
-	private Auditor auditor;
-
-	@Inject
 	private MyLogger logger;
 
+	@Auditable
 	public void finishCheckout() {
 		logger.log("Finishing Checkout");
-
-		auditor.audit();
 	}
 
 }
