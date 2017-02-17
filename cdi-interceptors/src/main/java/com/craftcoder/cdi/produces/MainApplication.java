566d9ac40f1fc6010710a4b8cb4fa7d0d2094a0f
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-17 09:06:29 -0200

    Creates code to get the first example of how to use CDI Interceptors

---
+++ cdi-interceptors/src/main/java/com/craftcoder/cdi/produces/MainApplication.java
@@ -0,0 +1,17 @@
+package com.craftcoder.cdi.produces;
+
+import javax.enterprise.inject.spi.CDI;
+
+import org.jboss.weld.environment.se.Weld;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		try (CDI<Object> container = new Weld().initialize()) {
+			Checkout checkout = container.select(Checkout.class).get();
+
+			checkout.finishCheckout();
+		}
+	}
+
+}
