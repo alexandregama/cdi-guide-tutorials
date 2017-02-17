5ebc7443fdb55442ff3f668fa95b5964e3fa3ccf
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-17 09:22:08 -0200

    Creates the @Loggin annotation to test CDI Interceptors

--- cdi-interceptors/src/main/java/com/craftcoder/cdi/produces/Checkout.java
+++ cdi-interceptors/src/main/java/com/craftcoder/cdi/produces/Checkout.java
@@ -1,14 +1,10 @@
 package com.craftcoder.cdi.produces;
 
-import javax.inject.Inject;
-
 public class Checkout {
 
-	@Inject
-	private MyLogger logger;
-
+	@Logging(message = "Finishing Checkout")
 	public void finishCheckout() {
-		logger.log("Finishing Checkout");
+		//your awesome business logic here!
 	}
 
 }
